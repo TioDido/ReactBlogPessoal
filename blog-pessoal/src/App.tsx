615be8766f0da5925069b0be6a365b5c67a298d9
TioDido <dougalonso1012@gmail.com>
2022-11-23 12:30:39 -0300

    segundoCommit

---
+++ blog-pessoal/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './paginas/home/home';
+
+function App() {
+  return (
+    <>
+      <Home />
+      
+    </>
+  );
+}
+
+export default App;
---
+++ blog-pessoal/src/paginas/home/home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './home.css'
+
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo'>Daniel Paladino na minha frente, gente.</h1>
+            <div className='container'>
+                <img className='img' src='https://i.ytimg.com/vi/K4czQ_TffSY/maxresdefault.jpg' alt='JesusBrabao'/>
+            </div>
+        </>
+    )
+}
+
+export default Home
