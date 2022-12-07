38c4aa3d9bb37da69cc6bdb4e213e651141b292b
TioDido <dougalonso1012@gmail.com>
2022-12-07 12:29:34 -0300

    redux

---
+++ blog-pessoal/src/App.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './paginas/home/home';
+import {Grid} from '@material-ui/core'
+import Navbar from './componentes/estaticos/navbar/navbar';
+import Footer from './componentes/estaticos/footer/footer';
+import Login from './paginas/login/login';
+import CadastroUsuario from './paginas/cadastro/CadastroUsuario'
+import ListaTema from './componentes/temas/listatemas/ListaTema';
+import ListaPostagem from './componentes/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './componentes/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './componentes/temas/cadastroTema/CadastroTema';
+import DeletarTema from './componentes/temas/deletarTema/DeletarTema';
+import DeletarPostagem from './componentes/postagens/deletarPostagem/DeletarPostagem';
+import { Provider } from 'react-redux';
+import store from './store/store';
+
+
+
+
+
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+       
+          <div style={{ minHeight: '100vh' }}>
+          <Routes>
+
+            <Route path="/" element={<Login />} />
+
+            <Route path="/login" element={<Login />} />
+
+            <Route path="/home" element={<Home />} />
+
+            <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+
+            <Route path="/temas" element={<ListaTema />} />
+
+            <Route path="/posts" element={<ListaPostagem />} />
+
+            <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+            <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+            <Route path="/formularioTema" element={<CadastroTema />} />
+
+            <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+            <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+            <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            </Routes>
+          </div>
+      
+        <Footer />
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
+
